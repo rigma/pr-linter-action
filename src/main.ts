ed88da65a2be3afb00a7a064afae2e0555ce2d28
rigma <rigbuntu@gmail.com>
2019-09-22 15:16:29 +0200

    refactor: putting action params into an interface

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import * as core from '@actions/core'
 import { GitHub, context } from '@actions/github'
+import { ActionParams } from './types/params'
 
-const params = {
-  repoToken: core.getInput('repo-token', {
+const params: ActionParams = {
+  repositoryToken: core.getInput('repo-token', {
     required: true
   })
 }
-const client = new GitHub(params.repoToken)
+const client = new GitHub(params.repositoryToken)
