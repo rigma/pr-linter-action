b99cfc354458970531de4c5bfef23c15d6f5028d
rigma <rigbuntu@gmail.com>
2019-09-22 16:19:20 +0200

    feat: add basic action script

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,49 @@
+import { context } from '@actions/github'
 import * as core from '@actions/core'
-import { GitHub, context } from '@actions/github'
-import { ActionParams } from './types/params'
+import * as load from '@commitlint/load'
+import * as lint from '@commitlint/lint'
 
-const params: ActionParams = {
-  repositoryToken: core.getInput('repo-token', {
-    required: true
-  })
+async function getPullTitle (): Promise<string> {
+  if (!context.payload.pull_request) {
+    throw new Error('Missing Pull Request in the current context')
+  }
+
+  return context.payload.pull_request.title
 }
-const client = new GitHub(params.repositoryToken)
+
+async function run (): Promise<void> {
+  // Setting the default configuration for PR linting
+  core.debug('Loading commintlint configuration')
+  const lintOptions = await load({
+    extends: ['@commitlint/config-conventional']
+  })
+
+  // Retrieving current PR title
+  let title: string
+  try {
+    core.debug('Retrieving PR title')
+    title = await getPullTitle()
+  } catch (err) {
+    core.error(err.message)
+    core.setFailed('Could not to retrieve Pull Request from your Actions\' context')
+
+    console.log('Could not to retrieve Pull Request from your Actions\' context')
+    return
+  }
+
+  // Generating linter report
+  core.debug('Generating commitlint report')
+  const lintReport = await lint(title, lintOptions.rules, {})
+
+  // If linter's report is fine, then it's the end of this action
+  if (lintReport.valid) {
+    return
+  }
+
+  lintReport.errors.forEach(error => {
+    core.error(`[${error.name}] ${error.message}`)
+  })
+  core.setFailed(`"${title}" is not a valid Pull Request title!`)
+}
+
+run()
